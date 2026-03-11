8ff56a74d9256dcb9320a31c997ce7c9dde3c7e0
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-03-11 13:22:01 +0530

    feat: add BatchController, BatchService interface, and BatchServiceImpl class

---
+++ src/main/java/com/fourbitlabs/employee_management_system/controller/BatchController.java
@@ -0,0 +1,4 @@
+package com.fourbitlabs.employee_management_system.controller;
+
+public class BatchController {
+}
---
+++ src/main/java/com/fourbitlabs/employee_management_system/service/BatchSevice.java
@@ -0,0 +1,4 @@
+package com.fourbitlabs.employee_management_system.service;
+
+public interface BatchSevice {
+}
