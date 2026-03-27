84893f0090e56eaf6f3266e97206613883d6994e
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-03-27 11:29:04 +0530

    feat: Implement Spring Security user details service, define authentication service interface, and add production environment configuration.

--- src/main/java/com/fourbitlabs/employee_management_system/service/interfaces/AuthService.java
+++ src/main/java/com/fourbitlabs/employee_management_system/service/interfaces/AuthService.java
@@ -3,11 +3,12 @@ package com.fourbitlabs.employee_management_system.service.interfaces;
 import com.fourbitlabs.employee_management_system.dto.request.LoginRequestDto;
 import com.fourbitlabs.employee_management_system.dto.response.LoginResponseDto;
 
-import com.fourbitlabs.employee_management_system.dto.request.RefreshTokenRequestDto;
 import com.fourbitlabs.employee_management_system.dto.response.TokenRefreshResponseDto;
 
 public interface AuthService {
     LoginResponseDto login(LoginRequestDto loginRequestDto);
+
     TokenRefreshResponseDto refreshToken(String refreshToken);
+
     void logout(String refreshToken);
 }
--- src/main/java/com/fourbitlabs/employee_management_system/utils/MyUserDetailsService.java
+++ src/main/java/com/fourbitlabs/employee_management_system/utils/MyUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.fourbitlabs.employee_management_system.utils;
 
 import com.fourbitlabs.employee_management_system.entity.User;
-import com.fourbitlabs.employee_management_system.enums.Role;
 import com.fourbitlabs.employee_management_system.exception.ResourceNotFoundException;
 import com.fourbitlabs.employee_management_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,8 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
