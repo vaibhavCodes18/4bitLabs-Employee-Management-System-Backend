066579b610d9d18fa7e22a4215c60ec845b88860
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-03-27 11:46:59 +0530

    feat: Add CORS configuration to allow requests from specified origins and methods.

--- src/main/java/com/fourbitlabs/employee_management_system/config/CorsConfig.java
+++ src/main/java/com/fourbitlabs/employee_management_system/config/CorsConfig.java
@@ -1,14 +1,9 @@
 package com.fourbitlabs.employee_management_system.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -19,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000",
+                                "https://4bitlabs-ems.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true)
